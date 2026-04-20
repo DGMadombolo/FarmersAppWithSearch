f84e7cc0ea1180756c49adb68f7159c83ba2246b
DGMadombolo <ST10537111@rcconnect.edu.za>
2026-04-20 11:33:01 +0200

    "Initial Commit"

---
+++ FarmersAppWithSearch/Models/AzureBlobStorageSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace FarmersAppWithSearch.Models
+{
+    public class AzureBlobStorageSettings
+    // connection string and container name for Azure storage account
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+
+
+        // Name of the blob container
+        public string ContainerName { get; set; } = string.Empty;
+    }
+}
--- FarmersAppWithSearch/Models/FarmerViewModel.cs
+++ FarmersAppWithSearch/Models/FarmerViewModel.cs
@@ -1,6 +1,30 @@
-﻿namespace FarmersAppWithSearch.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FarmersAppWithSearch.Models
 {
     public class FarmerViewModel
     {
+        public int Id { get; set; }
+
+        [Required]
+
+        public string FullName { get; set; } = string.Empty;
+
+        [Required]
+
+        public string FarmerCode { get; set; } = string.Empty;
+
+        public string? PhoneNumber { get; set; }
+
+        public string? FarmName { get; set; }
+
+        public string? Location { get; set; }
+
+        // store blob image URL in the database
+
+        public string? ImageUrl { get; set; }
+
+        public IFormFile? ImageFile { get; set; }   
     }
 }
+
